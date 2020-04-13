78ed223ab0cfa815bd7987ed5875629f8d7d96be
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-04-13 16:28:50 +0700

    7

--- work.py
+++ work.py
@@ -1,21 +1,41 @@
-import math
 
 zv = ''
+import math
 
-d = {'1': (3, 'АИ-80'), '2': (2, 'АИ-92'), '3': (4, 'АИ-92')}
-d1 = {'АИ-92': (3, 4, 2, 2), 'АИ-95': (3, 4), 'АИ-80': (1, 3), 'АИ-98': (3, 4)}
-list = [['1', '3', 'АИ-80'], ['2', '2', 'АИ-92'], ['3', '4', 'АИ-92', 'АИ-95', 'АИ-98']]
-list1 = [['00:01', '10', 'АИ-80'], ['00:04', '45', 'АИ-95'], ['00:12', '40', 'АИ-92'], ['00:41', '30', 'АИ-98'],
-         ['00:54', '25', 'АИ-98'], ['01:05', '25', 'АИ-92'], ['01:16', '45', 'АИ-92'], ['01:25', '50', 'АИ-98'],
-         ['01:26', '15', 'АИ-98'],
-         ['01:39', '40', 'АИ-80'], ['01:44', '30', 'АИ-95'], ['01:53', '20', 'АИ-92']]
+list = []
+list1 = []
+d = {}
+with open('azs.txt') as azs_file:  # достали из файла азс, сделали список
+    for line in azs_file.readlines():
+        list.append(line.split())
+number_kolonok = len(list)
+
+d1 = {}
+for i in list:
+    d[i[0]] = int(i[1]), i[2]
+    for elem in i:
+        if elem[:2] == 'АИ':
+            d1[elem] = int(i[0]), int(i[1])
+a = tuple(list[1][:2])
+d1['АИ-92'] += int(a[0]), int(a[1])
 
 kol_avtom = len(d) + 1
+nezapravilsa = 0
+aivos = 0
+aidevvt = 0
+aidevvos = 0
+aidevpat = 0
 avtomat = 0
 oz_na_1 = 0  # очередь на 1 колонку
 oz_na_2 = 0  # очередь на 2 колонку
 oz_na_3 = 0  # очередь на 3 колонку
 oz = 0
+
+with open('input.txt') as inp_file:  # список из файла инпут
+    for line in inp_file.readlines():
+        list1.append(line.split())
+
+number_car = len(list)
 
 with open('output.txt', 'w') as file_out:
     for klient in list1:
@@ -33,7 +53,7 @@ with open('output.txt', 'w') as file_out:
         if toplivo == 'АИ-80':
             oz_na_1 += 1
             if oz_na_1 <= d1['АИ-80'][1]:  # добавить время
-
+                aivos += int(kol_litr)
                 avtomat = d1['АИ-80'][0]
                 print('В {} новый клиент: {} {} {} {} встал в очередь к автомату № {}'.format(time_go, time_go, toplivo,
                                                                                               kol_litr, time_zapr,
@@ -41,6 +61,7 @@ with open('output.txt', 'w') as file_out:
                 oz = oz_na_1
             elif oz_na_1 > d1['АИ-80'][1]:
                 oz_na_1 -= 1
+                nezapravilsa += 1
                 print('В  {}  новый клиент: {} {} {} {} не смог заправить автомобиль и покинул АЗС.'.format(time_go,
                                                                                                             time_go,
                                                                                                             toplivo,
@@ -54,17 +75,20 @@ with open('output.txt', 'w') as file_out:
             if oz_na_2 <= d1['АИ-92'][3]:
                 avtomat = d1['АИ-92'][2]
                 oz = oz_na_2
+                aidevvt += int(kol_litr)
                 print('В {} новый клиент: {} {} {} {} встал в очередь к автомату № {}'.format(time_go, time_go, toplivo,
                                                                                               kol_litr, time_zapr,
                                                                                               avtomat), file=file_out)
             elif oz_na_2 > d1['АИ-92'][3] and oz_na_2 <= d1['АИ-92'][1]:
                 avtomat = d1['АИ-92'][0]
                 oz = oz_na_2
+                aidevvt += int(kol_litr)
                 print('В {} новый клиент: {} {} {} {} встал в очередь к автомату № {}'.format(time_go, time_go, toplivo,
                                                                                               kol_litr, time_zapr,
                                                                                               avtomat), file=file_out)
             elif oz_na_2 > d1['АИ-92'][1]:
                 oz_na_2 -= 1
+                nezapravilsa += 1
                 print('В  {}  новый клиент: {} {} {} {} не смог заправить автомобиль и покинул АЗС.'.format(time_go,
                                                                                                             time_go,
                                                                                                             toplivo,
@@ -81,8 +105,14 @@ with open('output.txt', 'w') as file_out:
                 print('В {} новый клиент: {} {} {} {} встал в очередь к автомату № {}'.format(time_go, time_go, toplivo,
                                                                                               kol_litr, time_zapr,
                                                                                               avtomat), file=file_out)
+                if toplivo == 'АИ-95':
+                    aidevpat += int(kol_litr)
+                elif toplivo == 'АИ-98':
+                    aidevvos += int(kol_litr)
+
             elif oz_na_3 > d1['АИ-95'][1]:
                 oz_na_3 -= 1
+                nezapravilsa += 1
                 print('В  {}  новый клиент: {} {} {} {} не смог заправить автомобиль и покинул АЗС.'.format(time_go,
                                                                                                             time_go,
                                                                                                             toplivo,
@@ -107,3 +137,8 @@ with open('output.txt', 'w') as file_out:
             print('Автомат № {} максимальная очередь: {} Марки бензина: {} -> {}'.format(inform, max_o, mark_benz, zv),
                   file=file_out)
             number_car = len(list)
+print('Не заправились: ', nezapravilsa)
+print ('АИ-80 :', aivos )
+print ('АИ-92 :', aidevvt)
+print ('АИ-95 :', aidevpat)
+print ('АИ-98 :', aidevvos)
