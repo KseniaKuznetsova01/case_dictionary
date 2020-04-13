e12cf9872704aa8eefc81d28857e97f4dd897a22
KatyaPanukova <panyukowa-yekaterina@yandex.ru>
2020-04-13 15:56:32 +0300

    part

--- work.py
+++ work.py
@@ -15,6 +15,8 @@ avtomat = 0
 oz_na_1 = 0  # очередь на 1 колонку
 oz_na_2 = 0  # очередь на 2 колонку
 oz_na_3 = 0  # очередь на 3 колонку
+oz = 0
+
 with open('output.txt', 'w') as file_out:
     for klient in list1:
         time_go = klient[0]
@@ -38,6 +40,7 @@ with open('output.txt', 'w') as file_out:
                                                                                               avtomat), file=file_out)
                 oz = oz_na_1
             elif oz_na_1 > d1['АИ-80'][1]:
+                oz_na_1 -= 1
                 print('В  {}  новый клиент: {} {} {} {} не смог заправить автомобиль и покинул АЗС.'.format(time_go,
                                                                                                             time_go,
                                                                                                             toplivo,
@@ -50,15 +53,18 @@ with open('output.txt', 'w') as file_out:
             oz_na_2 += 1
             if oz_na_2 <= d1['АИ-92'][3]:
                 avtomat = d1['АИ-92'][2]
+                oz = oz_na_2
                 print('В {} новый клиент: {} {} {} {} встал в очередь к автомату № {}'.format(time_go, time_go, toplivo,
                                                                                               kol_litr, time_zapr,
                                                                                               avtomat), file=file_out)
             elif oz_na_2 > d1['АИ-92'][3] and oz_na_2 <= d1['АИ-92'][1]:
                 avtomat = d1['АИ-92'][0]
+                oz = oz_na_2
                 print('В {} новый клиент: {} {} {} {} встал в очередь к автомату № {}'.format(time_go, time_go, toplivo,
                                                                                               kol_litr, time_zapr,
                                                                                               avtomat), file=file_out)
             elif oz_na_2 > d1['АИ-92'][1]:
+                oz_na_2 -= 1
                 print('В  {}  новый клиент: {} {} {} {} не смог заправить автомобиль и покинул АЗС.'.format(time_go,
                                                                                                             time_go,
                                                                                                             toplivo,
@@ -71,10 +77,12 @@ with open('output.txt', 'w') as file_out:
             oz_na_3 += 1
             if oz_na_3 <= d1['АИ-95'][1]:
                 avtomat = d1['АИ-95'][0]
+                oz = oz_na_3
                 print('В {} новый клиент: {} {} {} {} встал в очередь к автомату № {}'.format(time_go, time_go, toplivo,
                                                                                               kol_litr, time_zapr,
                                                                                               avtomat), file=file_out)
             elif oz_na_3 > d1['АИ-95'][1]:
+                oz_na_3 -= 1
                 print('В  {}  новый клиент: {} {} {} {} не смог заправить автомобиль и покинул АЗС.'.format(time_go,
                                                                                                             time_go,
                                                                                                             toplivo,
@@ -89,11 +97,11 @@ with open('output.txt', 'w') as file_out:
             if len(avtomat_0) > 3:
                 mark_benz = avtomat_0[2] + ' ' + avtomat_0[3] + ' ' + avtomat_0[4]
 
-            if inform == 1:
+            if inform == '1':
                 zv = oz_na_1 * '*'
-            elif inform == 2:
+            elif inform == '2':
                 zv = oz_na_2 * '*'
-            elif inform == 3:
+            elif inform == '3':
                 zv = oz_na_3 * '*'
 
             print('Автомат № {} максимальная очередь: {} Марки бензина: {} -> {}'.format(inform, max_o, mark_benz, zv),
